950ebaddff829c4c25fa3833c93a3032d4cdb975
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-26 13:53:03 +0430

    add section in layout and show it in other pages if you call those

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,3 +4,13 @@
 @section('content')
     <h1>Home</h1>    
 @endsection
+
+
+@section('sidebar')
+@parent
+<h4 style="color: green">this text  part of side bar in home page</h4>
+@endsection
+    
+
+    
+ 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,5 +10,14 @@
     
     @yield('content')
 
+    @section('sidebar')
+    <div class="sidebar">
+        <h3>Side Bar</h3>    
+        <h5 style="color: red"> this is a sidebar in the layout</h5>
+        @show
+    </div>
+  
+
+
 </body>
 </html>
