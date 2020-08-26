6caf91fc54479c321fface847d2e03ab2cbec8b1
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-26 13:58:37 +0430

    use @include to minimal code in page and call another page fully

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,8 @@
+
+
+@section('sidebar')
+    <div class="sidebar">
+    <h3>Side Bar</h3>    
+    <h5 style="color: red"> this is a sidebar in the layout</h5>
+    @show
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,7 @@
     
     @yield('content')
 
-    @section('sidebar')
-    <div class="sidebar">
-        <h3>Side Bar</h3>    
-        <h5 style="color: red"> this is a sidebar in the layout</h5>
-        @show
-    </div>
+   @include('inc.sidebar')
   
 
 
