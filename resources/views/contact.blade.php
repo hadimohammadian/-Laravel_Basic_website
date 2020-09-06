828cf225691f833b4983e135b48cf14c8a39ef3f
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-09-06 11:48:46 +0430

    Create bootstrap form for contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,31 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Contact Page</h1>
+<form>
+    <div class="form-group">
+      <label for="name">name</label>
+      <input type="text" class="form-control" id="name" name="name" >
+       
+    </div>
+     
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" >
+        
+      </div>
+
+      <div class="form-group">
+        <label for="subject">Subject</label>
+        <input type="text" class="form-control" id="subject"  name="subject">
+         
+      </div>
+
+      <div class="form-group">
+        <label for="message">message</label>
+        <textarea name="message" id="message" cols="30" rows="5" class="form-control"></textarea>
+        
+      </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 @endsection
