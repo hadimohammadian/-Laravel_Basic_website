7e92498b27560cbece34a020562f5fa139decb34
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-09-07 14:27:31 +0430

    get all data in message page

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -2,4 +2,22 @@
 
 @section('content')
     
+    @foreach ($messages as $item)
+
+    <ul class="list-group p-2" >
+        <li class="list-group-item">
+                {{$item->name}}
+        </li>
+        <li class="list-group-item">
+            {{$item->email}}
+    </li>
+    <li class="list-group-item">
+        {{$item->subject}}
+</li>
+<li class="list-group-item">
+    {{$item->message}}
+</li>
+    </ul>
+        
+    @endforeach
 @endsection
