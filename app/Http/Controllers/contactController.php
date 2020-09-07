fe30afc4eaae077b03f0b7d151e16ebfc6281b87
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-09-07 12:25:06 +0430

    save all gaving data from form at database

--- app/Http/Controllers/contactController.php
+++ app/Http/Controllers/contactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
+use App\Models\message;
 use Illuminate\Http\Request;
 use Symfony\Component\Console\Input\Input;
 
@@ -27,6 +28,14 @@ class contactController extends Controller
     
     public function submit(ContactRequest $request){
 
-        dd($request->input('name'));
-   }
+            $message = new message();
+
+            $message->name = $request->input('name');
+            $message->email = $request->input('email');
+            $message->subject = $request->input('subject');
+            $message->message = $request->input('message');
+            
+            $message->save();
+
+    }
 }
